ab6bd4e37a059682498d9d2634cfbb5c73e7367e
Sanhavat <bookbookboo123@gmail.com>
2026-04-28 14:30:13 +0700

    added diaryEditor

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
 import SideBar from './components/SideBar'
+import DiaryEditor from './components/DiaryEditor';
 
 export default function App() {
 
+  const handleSaveData = (data) => {
+    console.log("ได้รับข้อมูลแล้ว:", data);
+  };
+
   return (
     <>
-     <SideBar/>
+      <div className="flex-col">
+        <SideBar />
+        <div className="p-10">
+          <DiaryEditor onCreate={handleSaveData} />
+        </div>
+      </div>
+
     </>
+
   )
 }
 
